b82f45d863664f6be458c868b70cfc77f8e4c2e2
Siarhei Zuyeu <siarhei_zuyeu@hotmail.com>
2017-05-28 04:57:13 +0300

    alphabet sort of countries

--- src/main/java/com/rozenkow/service/GeoServiceImpl.java
+++ src/main/java/com/rozenkow/service/GeoServiceImpl.java
@@ -3,9 +3,11 @@ package com.rozenkow.service;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Locale;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Created by Poul Rozenkow.
@@ -14,13 +16,17 @@ import java.util.Map;
 public class GeoServiceImpl implements GeoService {
   @Override
   public Map<String, String> getLocalizedCountries() {
-    Map<String, String> countries = new LinkedHashMap<>();
+    Map<String, String> countries = new HashMap<>();
     Locale displayLocale = LocaleContextHolder.getLocale();
-    for (String countryCode : Locale.getISOCountries()) {
+    String[] isoCountries = Locale.getISOCountries();
+    for (String countryCode : isoCountries) {
       Locale locale = new Locale("", countryCode);
 
       countries.put(locale.getCountry(), locale.getDisplayCountry(displayLocale));
     }
+    countries = countries.entrySet().stream()
+        .sorted((o1, o2) -> o1.getValue().compareToIgnoreCase(o2.getValue()))
+        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (u, u2) -> u, LinkedHashMap::new));
     return countries;
   }
 }
